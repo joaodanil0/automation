bd1b6b3ec1becb365ac061e5b8f09b0a17185035
Joao Danilo <joaodanilo1992@gmail.com>
2025-10-03 20:57:26 -0400

    Loading global variables from environment variables

--- conftest.py
+++ conftest.py
@@ -1,6 +1,11 @@
 import pytest
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def pytest_configure(config):
-    pytest.MD_serialno = "test"
-    pytest.HU_serialno = "test2"
+    pytest.MD_serial_number = os.getenv("MD_serial_number")
+    pytest.HU_serial_number = os.getenv("HU_serial_number")

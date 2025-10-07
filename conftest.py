a08279ca4cbf382d56dc0122b14cecb6c017c4a0
Joao Danilo <joaodanilo1992@gmail.com>
2025-10-07 05:22:35 -0400

    Checked if serial number of devices were configured in

--- conftest.py
+++ conftest.py
@@ -7,5 +7,13 @@ load_dotenv()
 
 
 def pytest_configure(config):
-    pytest.MD_serial_number = os.getenv("MD_serial_number")
-    pytest.HU_serial_number = os.getenv("HU_serial_number")
+    
+    if os.getenv("MD_serial_number"):
+        pytest.MD_serial_number = os.getenv("MD_serial_number")
+    else:
+        print("MD serial number not configured. Please check the `.env` file")
+
+    if os.getenv("HU_serial_number"):
+        pytest.HU_serial_number = os.getenv("HU_serial_number")
+    else:
+        print("HU serial number not configured. Please check the `.env` file")

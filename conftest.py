5f916f043c9f1207e05b69f67fc9581e776f81a4
Joao Danilo <joaodanilo1992@gmail.com>
2025-10-07 06:44:23 -0400

    Added log feature

--- conftest.py
+++ conftest.py
@@ -2,18 +2,30 @@ import pytest
 import os
 
 from dotenv import load_dotenv
+from src.automation.util.Log import Log
 
 load_dotenv()
 
 
 def pytest_configure(config):
+
+    if os.getenv("LOG_LEVEL"):
+        pytest.LOG_LEVEL = os.getenv("LOG_LEVEL")
+    else:
+        pytest.LOG_LEVEL = "INFO"
     
+    log = Log()
+    logger = log.getLogger(__name__)
     if os.getenv("MD_serial_number"):
         pytest.MD_serial_number = os.getenv("MD_serial_number")
+        logger.debug("MD_serial_number env variables loaded!")
     else:
-        print("MD serial number not configured. Please check the `.env` file")
+        logger.error("MD serial number not configured. Please check the `.env` file")
 
     if os.getenv("HU_serial_number"):
         pytest.HU_serial_number = os.getenv("HU_serial_number")
+        logger.debug("HU_serial_number env variables loaded!")
     else:
-        print("HU serial number not configured. Please check the `.env` file")
+        logger.error("HU serial number not configured. Please check the `.env` file")
+
+    
---
+++ src/automation/util/Log.py
@@ -0,0 +1,33 @@
+
+import logging
+import pytest
+
+from io import StringIO
+from rich.console import Console
+from rich.logging import RichHandler
+
+class Log:
+
+    def __init__(self):
+        self.file_handler = logging.FileHandler("logs.txt")
+        # self.file_handler.setLevel(pytest.LOG_LEVEL)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.file_handler.setFormatter(formatter)
+        # FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        # logging.basicConfig(
+        #     level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+        # )
+        self.rich_handler = RichHandler(
+            show_time=True,
+            show_level=True,
+            show_path=False, # Or True, depending on your preference
+            rich_tracebacks=True
+        )
+
+    
+    def getLogger(self, name):
+        logger = logging.getLogger(name)
+        logger.setLevel(pytest.LOG_LEVEL)
+        logger.addHandler(self.rich_handler)
+        logger.addHandler(self.file_handler)
+        return logger
